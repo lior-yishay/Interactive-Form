847bc57f8f5d306d1e4ae094c3e3aa475b4f633b
lior yishay <lioryishay@gmail.com>
2025-07-08 22:10:53 +0300

    back to start

--- docs/scene-managment/sceneOrder.js
+++ docs/scene-managment/sceneOrder.js
@@ -17,7 +17,7 @@ import {
 import { resetDrawingState } from "../utils/resetDrawingState.js";
 import { p5Functions } from "./p5-scene-functions.js";
 
-let currentScene = I_BELIEVE_IN;
+let currentScene = START;
 export const getCurrentScene = () => currentScene;
 
 export const nextScene = () => {
