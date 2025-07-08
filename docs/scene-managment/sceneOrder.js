d90794a2a3a0b82fe73f65cc3ba45c1e41285e40
lior yishay <lioryishay@gmail.com>
2025-07-08 11:44:21 +0300

    changing back to start scene

--- docs/scene-managment/sceneOrder.js
+++ docs/scene-managment/sceneOrder.js
@@ -17,7 +17,7 @@ import {
 import { resetDrawingState } from "../utils/resetDrawingState.js";
 import { p5Functions } from "./p5-scene-functions.js";
 
-let currentScene = NAME;
+let currentScene = START;
 export const getCurrentScene = () => currentScene;
 
 export const nextScene = () => {
