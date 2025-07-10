4f0b38a14b547ebc4a75ccd7b9ab7b558782a5de
lior yishay <lioryishay@gmail.com>
2025-07-10 22:19:56 +0300

    fixing imports

--- server/business/scenes-logic/big thing/api.js
+++ server/business/scenes-logic/big thing/api.js
@@ -1,6 +1,6 @@
-import { BIG_THING_COLLECTION } from "../../../data-access/collections";
-import { getScenePickManager } from "../../scenes-managers/pickManager";
-import { BIG_THING_OPTIONS } from "./options";
+import { BIG_THING_COLLECTION } from "../../../data-access/collections.js";
+import { getScenePickManager } from "../../scenes-managers/pickManager.js";
+import { BIG_THING_OPTIONS } from "./options.js";
 
 const bigThingSceneManager = getScenePickManager(BIG_THING_COLLECTION);
 
